76d201edf9344a26651fe5d46c97e7fff82d2a17
Aravind207 <sathyanarayanan.ar@northeastern.edu>
2023-11-21 13:35:32 -0500

    Added routes for Blogs!!

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import * as userController from "../controllers/user-controller.js";
 import * as userProfileController from '../controllers/userProfileController.js';
+import * as BlogController from '../controllers/blog-controller.js';
 
 const router = express.Router();
 
@@ -10,11 +11,19 @@ router.route('/user/sign-up')
 router.route('/user/:id')
     .get(userController.get);
 
-    router.route('/userProfile')   
+router.route('/userProfile')   
     .post(userProfileController.post)
 
 router.route('/userProfile/:id')
     .get(userProfileController.get)
     .put(userProfileController.put)
 
+router.route('/blogs')
+    .get(BlogController.get)
+    .post(BlogController.post);
+
+router.route('/blogs/:id')
+    .put(BlogController.put)
+    .delete(BlogController.remove);
+  
 export default router;
