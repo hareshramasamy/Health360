eba3e622e590173bbc3ee5e684611de283ae31bc
Akash Arumugam <venkatachalapathy.a@northeastern.edu>
2023-12-06 01:03:51 -0500

    Added Diet plan page and integrated with backend

--- frontend/src/components/DietPlan/DietPlanDashboard.tsx
+++ frontend/src/components/DietPlan/DietPlanDashboard.tsx
@@ -1,8 +1,14 @@
 import React from "react"
 import "./DietPlanDashboard.css"
 import Header from "../LandingPage/Header"
+import {useNavigate} from 'react-router-dom';
 
 const DietPlanDashboard = () => {
+    let navigate = useNavigate(); 
+    const routeChange = (path: any) =>{  
+      navigate(path);
+    }
+
     return (
         <div>
             <Header />
@@ -10,9 +16,9 @@ const DietPlanDashboard = () => {
             <div className="dietplan-container">
                 <p className="dietplan">Diet Plan</p>
                 <div>
-                    <button className="options">Vegetarian</button>
-                    <button className="options">Non-Vegetarian</button>
-                    <button className="options">Vegan</button>
+                    <button className="options" onClick={() => routeChange('vegetarian')}>Vegetarian</button>
+                    <button className="options" onClick={() => routeChange('nonvegetarian')}>Non-Vegetarian</button>
+                    <button className="options" onClick={() => routeChange('vegan')}>Vegan</button>
                 </div>
                 
             </div>
