3fcde42ab47d1f86e6ce464626cf0bd7536d8321
Aravind207 <sathyanarayanan.ar@northeastern.edu>
2023-12-08 12:31:25 -0500

    Added Stylling to the blog page

--- frontend/src/components/BlogPage/BlogHeading.tsx
+++ frontend/src/components/BlogPage/BlogHeading.tsx
@@ -1,16 +1,25 @@
-import Landing from "../LandingPage/Landing";
+import { useNavigate } from "react-router-dom";
 import "./BlogPage.css";
 
 const Header = () => {
+  const navigate = useNavigate();
+
+  const handleCreateBlog = () => {
+    // Navigate to the create blog page
+    navigate("/blogs/create");
+  };
+
   return (
     <div className="blogTitle">
-            <div>
-            <h1 className="typing-animation">Blog Your Ideas!</h1>
-            </div>
-            <div>
-            <button className="create">Create Blog</button>
-            </div>
-        </div>
+      <div>
+        <h1 className="typing-animation">Blog Your Ideas!</h1>
+      </div>
+      <div>
+        <button className="create" onClick={handleCreateBlog}>
+          Create Blog
+        </button>
+      </div>
+    </div>
   );
 };
 
