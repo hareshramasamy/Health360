c8ceb8959c43e8e2e35e727e8d11408ea0bfb28c
Aravind207 <sathyanarayanan.ar@northeastern.edu>
2023-12-08 12:27:33 -0500

    Added Create Blog Functionality for the Blogs

--- frontend/src/components/BlogPage/CreatePost.tsx
+++ frontend/src/components/BlogPage/CreatePost.tsx
@@ -11,6 +11,7 @@ const CreatePost: React.FC<CreatePostProps> = () => {
   const [title, setTitle] = useState<string>("");
   const [summary, setSummary] = useState<string>("");
   const [content, setContent] = useState<string>("");
+  const [author, setAuthor] = useState<string>("");
   // const [files, setFiles] = useState<File[]>([]);
 const navigate = useNavigate();
   const modules = {
@@ -38,7 +39,7 @@ const navigate = useNavigate();
       title: title,
       summary: summary,
       description: content,
-      author :"Boron"
+      author : author,
 
     };
   
@@ -69,182 +70,54 @@ const navigate = useNavigate();
 
   return (
     <form className="createpost" onSubmit={createNewPost}>
-      <input
+      {/* <h1>Create Post</h1> */}
+      <br />
+      {/* <input
         type="text"
+        className="title"
         placeholder={"Title"}
         value={title}
         onChange={(ev: ChangeEvent<HTMLInputElement>) => setTitle(ev.target.value)}
-      />
-      <input
-        type="summary"
-        placeholder={"Summary"}
-        value={summary}
-        onChange={(ev: ChangeEvent<HTMLInputElement>) => setSummary(ev.target.value)}
-      />
-      {/* <input
-        type="file"
-        placeholder={"Image"}
-        onChange={(ev: ChangeEvent<HTMLInputElement>) => setFiles(ev.target.files as File[])}
       /> */}
-      <ReactQuill
+      <input
+        type="text"
+        className="title"
+        placeholder={"Title"}
+        value={title}
+        onChange={(ev: ChangeEvent<HTMLInputElement>) => setTitle(ev.target.value)}
+        required
+      /> 
+      <textarea
+        placeholder="Content"
+        value={content}
+        onChange={(ev: ChangeEvent<HTMLTextAreaElement>) => setContent(ev.target.value)}
+        required
+        rows={4} // Adjust the number of rows as needed 
+        cols={100} // Adjust the number of columns as needed
+        ></textarea>
+      <input
+        type="author"
+        placeholder={"Author"}
+        value={author}
+        onChange={(ev: ChangeEvent<HTMLInputElement>) => setAuthor(ev.target.value)}
+        required
+      />
+
+      <button className = "submit" type="submit">Create Post</button>
+
+      {/* <ReactQuill
         theme="snow"
         placeholder={"Content"}
         value={content}
-        onChange={(newValue: string) => setContent(newValue)}
+        onChange={(newValue: string) => {
+          
+          setContent(newValue)
+        }}
         modules={modules}
         formats={formats}
-      />
-      <button type="submit">Create Post</button>
+      /> */}
     </form>
-  );
+      );
 };
 
 export default CreatePost;
-
-// import React, { useState, ChangeEvent, FormEvent } from "react";
-// import ReactQuill from "react-quill";
-// import "react-quill/dist/quill.snow.css";
-
-// interface CreatePostProps {}
-
-// const CreatePost: React.FC<CreatePostProps> = () => {
-//   const [title, setTitle] = useState<string>("");
-//   const [summary, setSummary] = useState<string>("");
-//   const [content, setContent] = useState<string>("");
-//   const [files, setFiles] = useState<File[]>([]);
-
-//   const modules = {
-//     toolbar: [
-//       [{ 'header': [1, 2, false] }],
-//       ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-//       [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
-//       ['link', 'image'],
-//       ['clean']
-//     ],
-//   };
-
-//   const formats = [
-//     'header',
-//     'bold', 'italic', 'underline', 'strike', 'blockquote',
-//     'list', 'bullet', 'indent',
-//     'link', 'image'
-//   ];
-
-//   async function createNewPost(ev: FormEvent) {
-//     ev.preventDefault();
-
-//     const data = new FormData();
-//     data.set('title', title);
-//     data.set('summary', summary);
-//     data.set('content', content);
-//     data.set('file', files[0]);
-
-//     const response = await fetch("http://localhost:3000/post", {
-//       method: "POST",
-//       body: data,
-//     });
-
-//     console.log(await response.json());
-//   }
-
-//   return (
-//     <form className="createpost" onSubmit={createNewPost}>
-//       <input
-//         type="text"
-//         placeholder={"Title"}
-//         value={title}
-//         onChange={(ev: ChangeEvent<HTMLInputElement>) => setTitle(ev.target.value)}
-//       />
-//       <input
-//         type="summary"
-//         placeholder={"Summary"}
-//         value={summary}
-//         onChange={(ev: ChangeEvent<HTMLInputElement>) => setSummary(ev.target.value)}
-//       />
-//       <input
-//         type="file"
-//         placeholder={"Image"}
-//         onChange={(ev: ChangeEvent<HTMLInputElement>) => setFiles(ev.target.files || [])}
-//       />
-//       <ReactQuill
-//         theme="snow"
-//         placeholder={"Content"}
-//         value={content}
-//         onChange={(newValue: string) => setContent(newValue)}
-//         modules={modules}
-//         formats={formats}
-//       />
-//       <button type="submit">Create Post</button>
-//     </form>
-//   );
-// };
-
-// export default CreatePost;
-
-
-
-// import React, { useState } from "react";
-// import ReactQuill from "react-quill";
-// import "react-quill/dist/quill.snow.css";
-
-
-
-// export default function CreatePost() {
-//     const [title, setTitle] = useState(' ');
-//     const [summary, setSummary] = useState(' ');
-//     const [content, setContent] = useState(' ');
-//     const [files, setFiles] = useState([]);
-
-//     const modules = {
-//         toolbar: [
-//           [{ 'header': [1, 2, false] }],
-//           ['bold', 'italic', 'underline','strike', 'blockquote'],
-//           [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
-//           ['link', 'image'],
-//           ['clean']
-//         ],
-//     };
-
-//     const formats = ['header',
-//     'bold', 'italic', 'underline', 'strike', 'blockquote',
-//     'list', 'bullet', 'indent',
-//     'link', 'image'
-//   ]
-//    client/src/Pages/CreatePost.js
-//   async function createNewPost(ev) {
-//     const data = new FormData();
-//     data.set('title', title);
-//     data.set('summary', summary);
-//     data.set('content', content);
-//     data.set('file', files[0]);
-//     ev.preventDefault();
-//     const response = await fetch("http://localhost:3000/post", {
-//         method: "POST",
-//         body: data,
-//         });
-//     console.log(await response.json());
-//   }
-//     return (
-//             <form className="createpost" onSubmit={createNewPost}>
-//                 <input  type="text" 
-//                         placeholder={"Title"}
-//                         value={title} 
-//                         onChange={ev => setTitle(ev.target.value)} />
-//                 <input type="summary" 
-//                        placeholder={"Summary"}
-//                        value={summary}
-//                        onChange={ev => setSummary(ev.target.value)} />
-//                 <input type="file"
-//                        placeholder={"Image"}
-//                     //    value={files}
-//                        onChange={ev => setFiles(ev.target.files) } />
-//                 <ReactQuill theme="snow" 
-//                             placeholder={"Content"} 
-//                             value={content} 
-//                             onChange={newValue => setContent(newValue)}
-//                             modules={modules} 
-//                             formats={formats} />
-//                 <button type="submit">Create Post</button>
-//             </form>
-//     )
-// }
